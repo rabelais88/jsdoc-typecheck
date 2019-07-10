b2211b4a182a9222f21f8ed2c0eebd7367301a60
rabelais88 <sungryeolp@gmail.com>
2019-07-10 14:34:13 +0900

    update

--- jsdocTs.js
+++ jsdocTs.js
@@ -7,7 +7,12 @@ const John = {
   age: '24',
 }
 
-/** @type {MyData.CallPerson} */
+/**
+ * @type {MyData.CallPerson}
+ * @description This is a function to call a person
+ * @example
+ * CallPerson('karl', 24);
+ * */
 const CallPerson = (name, age) => {
   console.log(name, age);
   return true;
